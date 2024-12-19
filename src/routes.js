ec8b05412dd0454a526dacaafe069d50c559c347
FabioLWT <fabiotamazzia15@gmail.com>
2024-12-18 23:15:01 -0300

    Sua mensagem de commit

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ const { Router } = require("express");
 
 const routes = Router();
 
-routes.get("/health", (req, res) => {
-  return res.status(200).json({ message: "Server on" });
+routes.get("/", (req, res) => {
+  return res.status(200).json({ message: "Bem-Vindo a Neo Quimica Arena!!!!" });
 });
 
 module.exports = routes;
